3cbd3cc16f888e211e17c34e932f213863d53163
MokhaLeee <nkulyc@163.com>
2026-03-31 14:33:09 +0800

    review shop

--- include/shop.h
+++ include/shop.h
@@ -8,91 +8,91 @@
 #define SHOP_ITEMS_MAX_AMT 20
 
 enum videoalloc_shop {
-    BGCHR_SHOP_TALKTEXT = 0x200,
-    BGPAL_SHOP_ICON = 4,
-    BGPAL_SHOP_MAINBG1 = 12,
-    BGPAL_SHOP_MAINBG2 = 14,
+	BGCHR_SHOP_TALKTEXT = 0x200,
+	BGPAL_SHOP_ICON = 4,
+	BGPAL_SHOP_MAINBG1 = 12,
+	BGPAL_SHOP_MAINBG2 = 14,
 
-    OBJCHR_SHOP_SPINARROW = 0x240,
-    OBJPAL_SHOP_SPINARROW = 3,
+	OBJCHR_SHOP_SPINARROW = 0x240,
+	OBJPAL_SHOP_SPINARROW = 3,
 
-    OBJCHR_SHOP_GOLDBOX = 0x260,
-    OBJPAL_SHOP_GOLDBOX = 4,
+	OBJCHR_SHOP_GOLDBOX = 0x260,
+	OBJPAL_SHOP_GOLDBOX = 4,
 };
 
 enum ShopProcLabel {
-    PL_SHOP_ENTRY = 0,
-    PL_SHOP_BUY,
-    PL_SHOP_BUY_MAIN,
-    PL_SHOP_BUY_DONE,
-    PL_SHOP_SELL,
-    PL_SHOP_SELL_MAIN,
-    PL_SHOP_6_UNUSED, // unused
-    PL_SHOP_SELL_NOITEM,
-    PL_SHOP_ANYTHING_ELSE,
-    PL_SHOP_SENDTO_INVENTORY,
-    PL_SHOP_SENDTO_INVENTORY_EXT,
-    PL_SHOP_BUY_FULL_NO_INEVNTORY,
-    PL_SHOP_EXIT,
+	PL_SHOP_ENTRY = 0,
+	PL_SHOP_BUY,
+	PL_SHOP_BUY_MAIN,
+	PL_SHOP_BUY_DONE,
+	PL_SHOP_SELL,
+	PL_SHOP_SELL_MAIN,
+	PL_SHOP_6_UNUSED, // unused
+	PL_SHOP_SELL_NOITEM,
+	PL_SHOP_ANYTHING_ELSE,
+	PL_SHOP_SENDTO_INVENTORY,
+	PL_SHOP_SENDTO_INVENTORY_EXT,
+	PL_SHOP_BUY_FULL_NO_INEVNTORY,
+	PL_SHOP_EXIT,
 };
 
 struct ProcShop {
-    /* 00 */ PROC_HEADER;
+	/* 00 */ PROC_HEADER;
 
-    /* 2C */ struct Unit *unit;
-    /* 30 */ u16 shopItems[20];
+	/* 2C */ struct Unit *unit;
+	/* 30 */ u16 shopItems[20];
 
-    /* 58 */ u16 unk_58;
+	/* 58 */ u16 unk_58;
 
-    /* 5A */ u8 shopItemCount;
-    /* 5B */ u8 unitItemCount;
-    /* 5C */ u8 head_loc;
-    /* 5D */ u8 hand_loc;
-    /* 5E */ u8 head_idx;
-    /* 5F */ u8 hand_idx; // maybe top visible item in menu?
-    /* 60 */ u8 buy_or_sel;
-    /* 61 */ u8 shopType;
-    /* 62 */ u8 helpTextActive;
+	/* 5A */ u8 shopItemCount;
+	/* 5B */ u8 unitItemCount;
+	/* 5C */ u8 head_loc;
+	/* 5D */ u8 hand_loc;
+	/* 5E */ u8 head_idx;
+	/* 5F */ u8 hand_idx; // maybe top visible item in menu?
+	/* 60 */ u8 buy_or_sel;
+	/* 61 */ u8 shopType;
+	/* 62 */ u8 helpTextActive;
 
-    /* 64 */ i16 goldbox_x, goldbox_y, goldbox_oam2;
+	/* 64 */ i16 goldbox_x, goldbox_y, goldbox_oam2;
 };
 
 enum ShopType {
-    /* ProcShop::shopType */
-    SHOP_TYPE_ARMORY      = 0,
-    SHOP_TYPE_VENDOR      = 1,
-    SHOP_TYPE_SECRET_SHOP = 2,
+	/* ProcShop::shopType */
+	SHOP_TYPE_ARMORY	  = 0,
+	SHOP_TYPE_VENDOR	  = 1,
+	SHOP_TYPE_SECRET_SHOP = 2,
 
-    SHOP_TYPE_MAX
+	SHOP_TYPE_MAX
 };
 
 enum ShopStatus {
-    /* ProcShop::buy_or_sel */
-    SHOP_ST_BUY,
-    SHOP_ST_SELL
+	/* ProcShop::buy_or_sel */
+	SHOP_ST_BUY,
+	SHOP_ST_SELL
 };
 
 struct ProcShopInit {
-    /* 00 */ PROC_HEADER;
+	/* 00 */ PROC_HEADER;
 
-    STRUCT_PAD(0x29, 0x54);
+	STRUCT_PAD(0x29, 0x54);
 
-    /* 54 */ struct ProcShop * shopproc;
+	/* 54 */ struct ProcShop *shopproc;
 };
 
 typedef void (* ShopFunc)(struct ProcShop *, int);
 
 struct ShopState {
-    /* 00 */ u16 head_loc;
-    /* 02 */ u16 item_cnt;
-    /* 04 */ u16 lines;
-    /* 06 */ u16 hand_loc;
-    /* 08 */ u16 px_per_line;
-    /* 0A */ u16 trig;
-    /* 0C */ u16 bg2_off;
-    /* 10 */ int bg2_base;
-    /* 14 */ ShopFunc draw_line;
-    /* 18 */ ProcPtr proc;
+	/* 00 */ u16 head_loc;
+	/* 02 */ u16 item_cnt;
+	/* 04 */ u16 lines;
+	/* 06 */ u16 hand_loc;
+	/* 08 */ u16 px_per_line;
+	/* 0A */ u16 trig;
+	/* 0C */ u16 bg2_off;
+	/* 10 */ int bg2_base;
+	/* 14 */ ShopFunc draw_line;
+	/* 18 */ ProcPtr proc;
 };
 extern CONST_DATA struct ShopState *gpShopSt;
 
@@ -103,11 +103,11 @@ extern struct Text Text_GoldBox;
 int GetShopFace(struct ProcShop *proc);
 void StartShopDialogue(int msg, struct ProcShop *proc);
 void StartDefaultArmoryScreen(struct Unit *unit, ProcPtr proc);
-void StartArmoryScreenOrphaned(struct Unit *unit, u16 const * iid_list);
-void StartVendorScreenOrphaned(struct Unit *unit, u16 const * iid_list);
-void StartSecretShopScreenOrphaned(struct Unit *unit, u16 const * iid_list);
-// StartArmoryScreen
-void StartShopScreen(struct Unit *unit, const u16 * iid_list, u8 type, ProcPtr parent);
+void StartArmoryScreenOrphaned(struct Unit *unit, u16 const *iid_list);
+void StartVendorScreenOrphaned(struct Unit *unit, u16 const *iid_list);
+void StartSecretShopScreenOrphaned(struct Unit *unit, u16 const *iid_list);
+void StartArmoryScreen(struct Unit *unit, u16 const *iid_list);
+void StartShopScreen(struct Unit *unit, const u16 *iid_list, u8 type, ProcPtr parent);
 void UpdateShopItemCounts(struct ProcShop *proc);
 void TalkChoice_OnBuy(void);
 void TalkChoice_OnSell(void);
@@ -145,12 +145,12 @@ void ClearGoldBoxTextTm2Line(u16 *tm, int lines);
 void DisplayGoldBoxText(u16 *tm);
 void ShopInitTexts_OnBuy(struct ProcShop *proc);
 void DrawShopSoldItems(struct ProcShop *proc);
-void InitShopBuyStatus(struct ProcShopInit * proc);
+void InitShopBuyStatus(struct ProcShopInit *proc);
 void ShopInitTexts_OnSell(struct ProcShop *proc);
 void ShopDrawDefaultSellItemLine(struct ProcShop *proc);
-void InitShopSellStatus(struct ProcShopInit * proc);
-void DrawShopItemPriceLine(struct Text * text, int item, struct Unit *unit, u16 * tm);
-void DrawShopItemLine(struct Text * text, int item, struct Unit *unit, u16 * tm);
+void InitShopSellStatus(struct ProcShopInit *proc);
+void DrawShopItemPriceLine(struct Text *text, int item, struct Unit *unit, u16 *tm);
+void DrawShopItemLine(struct Text *text, int item, struct Unit *unit, u16 *tm);
 u16 GetItemPurchasePrice(struct Unit *, int);
 u16 GetItemSellPrice(int);
 bool IsItemSellable(int item);
@@ -170,16 +170,16 @@ void Shop_TryMoveHandPage(void);
 u16 ShopSt_GetHeadLoc(void);
 int ShopSt_GetBg2Offset(void);
 u16 ShopSt_GetHandLoc(void);
-// ShopSt_SetLineHeight
-// ShopSt_SetSetPageScrollTrigOffset
+void ShopSt_SetLineHeight(int px);
+void ShopSt_SetSetPageScrollTrigOffset(int trig);
 bool IsShopPageScrolling(void);
 bool ShouldDisplayUpArrow(void);
 bool ShouldDisplayDownArrow(void);
-// func_fe6_08097E2C
+bool func_fe6_08097E2C(int a);
 
-// extern CONST_DATA ??? gDefaultShopItems
-// extern CONST_DATA ??? gShopDislogueIdx
-// extern CONST_DATA ??? gShopFaces
+extern u16 CONST_DATA gDefaultShopItems[];
+extern int CONST_DATA gShopDislogueIdx[SHOP_TYPE_MAX];
+extern int CONST_DATA gShopFaces[SHOP_TYPE_MAX];
 extern CONST_DATA struct ProcScr ProcScr_ShopFadeIn[];
 extern CONST_DATA struct ProcScr ProcScr_ShopFadeOut[];
 extern CONST_DATA struct ProcScr ProcScr_Shop[];
